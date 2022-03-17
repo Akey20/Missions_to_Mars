36edc4cf287df563e11eaba17ec7684b4191c309
unknown <april.key20@gmail.com>
2022-03-17 18:33:11 -0400

    adding files

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_pymongo import PyMongo
+from flask import Flask, render_template, redirect, url_for 
 import scrape_mars
+from flask_pymongo import PyMongo
+import json
+
 
 app  = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/" 
+mongo = PyMongo(app)  
 
 @app.route("/")
 def index():
@@ -10,12 +14,18 @@ def index():
 
 @app.route("/scrape")
 def scrape():
-     #test to call scrape_mars.py
-    mars_data = scrape_mars.scrape_all()
-    #print(mars_data)  
-    return mars_data
+    marsTable = mongo.db.marsData  
+    # mongo.db.marsData.drop()  
+    # test to call scrape_mars.py
+    mars_data = scrape_mars.scrape_all() 
+    # mars_d = json.dumps(mars_data)
+    # print(mars_data)  
+    # return mars_data
+
+    marsTable.insert_one(mars_data)
+    return mars_data  
     
 
     
 if __name__ == "__main__":
-    app.run()   
+    app.run(port=8000)   
